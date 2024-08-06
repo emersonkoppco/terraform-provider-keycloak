ad77144e6c38f48c04909d17788b0fd33c2c80d3
Emerson Kopp <15053421+emersonkopp@users.noreply.github.com>
2024-08-06 13:10:19 -0300

    default-default-client-scopes and default-optional-client-scopes

--- provider/resource_keycloak_openid_default_default_client_scope.go
+++ provider/resource_keycloak_openid_default_default_client_scope.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -69,30 +70,56 @@ func resourceKeycloakOpenidDefaultDefaultClientScopeReconcile(ctx context.Contex
 		return diag.FromErr(err)
 	}
 
-	diagnostics, done := detachDeletedDefaultScopes(ctx, keycloakOpenidDefaultDefaultScopes, tfOpenidDefaultDefaultScopes, err, keycloakClient, realmId)
-	if done {
+	diagnostics := detachDeletedDefaultScopes(ctx, keycloakOpenidDefaultDefaultScopes, tfOpenidDefaultDefaultScopes, err, keycloakClient, realmId)
+	if diagnostics != nil {
 		return diagnostics
 	}
 
 	if tfOpenidDefaultDefaultScopes.Len() > 0 {
-		return attachNewDefaultScopes(ctx, keycloakClient, realmId, tfOpenidDefaultDefaultScopes)
+		diagnostics = attachNewDefaultScopes(ctx, keycloakClient, realmId, tfOpenidDefaultDefaultScopes)
+		if diagnostics != nil {
+			return diagnostics
+		}
 	}
 
+	return waitForDefaultUpdates(ctx, keycloakClient, realmId, tfOpenidDefaultDefaultScopes, 5)
+}
+
+func waitForDefaultUpdates(ctx context.Context, keycloakClient *keycloak.KeycloakClient, realmId string, scopes *schema.Set, times int) diag.Diagnostics {
+	if times == 0 {
+		return nil
+	}
+	keycloakOpenidDefaultDefaultScopes, err := keycloakClient.GetOpenidRealmDefaultDefaultClientScopes(ctx, realmId)
+	if err != nil {
+		if keycloak.ErrorIs404(err) {
+			return diag.FromErr(fmt.Errorf("validation error: realm with id %s does not exist", realmId))
+		}
+		return diag.FromErr(err)
+	}
+
+	if len(keycloakOpenidDefaultDefaultScopes) != scopes.Len() {
+		time.Sleep(1 * time.Second)
+		return waitForOptionalUpdates(ctx, keycloakClient, realmId, scopes, times-1)
+	}
+	for _, keycloakOpenidDefaultDefaultScope := range keycloakOpenidDefaultDefaultScopes {
+		if !scopes.Contains(keycloakOpenidDefaultDefaultScope.Name) {
+			time.Sleep(1 * time.Second)
+			return waitForOptionalUpdates(ctx, keycloakClient, realmId, scopes, times-1)
+		}
+	}
 	return nil
 }
 
-func detachDeletedDefaultScopes(ctx context.Context, keycloakOpenidDefaultDefaultScopes []*keycloak.OpenidClientScope, tfOpenidDefaultDefaultScopes *schema.Set, err error, keycloakClient *keycloak.KeycloakClient, realmId string) (diag.Diagnostics, bool) {
+func detachDeletedDefaultScopes(ctx context.Context, keycloakOpenidDefaultDefaultScopes []*keycloak.OpenidClientScope, tfOpenidDefaultDefaultScopes *schema.Set, err error, keycloakClient *keycloak.KeycloakClient, realmId string) diag.Diagnostics {
 	for _, keycloakOpenidDefaultDefaultScope := range keycloakOpenidDefaultDefaultScopes {
-		if tfOpenidDefaultDefaultScopes.Contains(keycloakOpenidDefaultDefaultScope.Name) {
-			tfOpenidDefaultDefaultScopes.Remove(keycloakOpenidDefaultDefaultScope.Name)
-		} else {
+		if !tfOpenidDefaultDefaultScopes.Contains(keycloakOpenidDefaultDefaultScope.Name) {
 			err = keycloakClient.DeleteOpenidRealmDefaultDefaultClientScope(ctx, realmId, keycloakOpenidDefaultDefaultScope.Id)
 			if err != nil {
-				return diag.FromErr(err), true
+				return diag.FromErr(err)
 			}
 		}
 	}
-	return nil, false
+	return nil
 }
 
 func attachNewDefaultScopes(ctx context.Context, keycloakClient *keycloak.KeycloakClient, realmId string, tfOpenidDefaultDefaultScopes *schema.Set) diag.Diagnostics {
